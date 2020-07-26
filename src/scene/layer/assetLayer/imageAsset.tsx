b38bc59bf6c4cb8a0b21e31b4419c89e5b6a9bc7
Brandon Tuttle <11356668+tutman96@users.noreply.github.com>
2020-07-26 15:18:53 -0400

    Update baseUrl and cache image assets for better performance

--- src/scene/layer/assetLayer/imageAsset.tsx
+++ src/scene/layer/assetLayer/imageAsset.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Image } from 'react-konva';
 
 import TransformableAsset from '../../canvas/transformableAsset';
-import { useImageAssetFile } from '../../asset/image';
+import { useImageAsset } from '../../asset/image';
 import { IAssetComponentProps } from '.';
 import { IAsset } from '../../asset';
 
@@ -12,41 +12,25 @@ export interface IImageAsset extends IAsset {
 
 interface Props extends IAssetComponentProps<IImageAsset> { };
 const ImageAsset: React.SFC<Props> = ({ asset, onUpdate, selected, onSelected }) => {
-	const [imageEl, setImageEl] = useState<HTMLImageElement>();
-
-	const [file] = useImageAssetFile(asset);
-
-	useEffect(() => {
-		if (file) {
-			var fr = new FileReader();
-			const img = document.createElement('img') as HTMLImageElement;
-			fr.onload = function () {
-				if (fr.result) {
-					img.src = fr.result as string;
-					setImageEl(img);
-				}
-			}
-			fr.readAsDataURL(file);
-		}
-	}, [file])
+	const imageEl = useImageAsset(asset);
 
 	return (
-			<TransformableAsset
-				isSelected={selected}
-				onSelected={onSelected}
-				rectTransform={asset.transform}
-				onTransform={(newRect) => {
-					onUpdate({
-						...asset,
-						transform: newRect
-					})
-				}}>
-				{imageEl && <Image
-					image={imageEl}
-					width={asset.transform.width}
-					height={asset.transform.height}
-				/>}
-			</TransformableAsset>
+		<TransformableAsset
+			isSelected={selected}
+			onSelected={onSelected}
+			rectTransform={asset.transform}
+			onTransform={(newRect) => {
+				onUpdate({
+					...asset,
+					transform: newRect
+				})
+			}}>
+			{imageEl && <Image
+				image={imageEl}
+				width={asset.transform.width}
+				height={asset.transform.height}
+			/>}
+		</TransformableAsset>
 	);
 }
 
