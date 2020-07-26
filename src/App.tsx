5058422459fe242e2b87164264a7ed5cb0e52018
tutman96 <11356668+tutman96@users.noreply.github.com>
2020-07-26 14:52:57 -0400

    Fix build issue and update build to use yarn

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import {
 	HashRouter as Router,
 	Switch,
-	Route,
-	Redirect
+	Route
 } from 'react-router-dom';
 import { DarkMode } from 'sancho';
 import { css } from 'emotion';
