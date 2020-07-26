c517ab6265f26bd056f2e9f4a1ac9d9bd584da30
Brandon Tuttle <68201364+brandontuttle@users.noreply.github.com>
2020-07-26 14:34:44 -0400

    Added display of grid to the table display

--- src/scene/canvas/index.tsx
+++ src/scene/canvas/index.tsx
@@ -1,73 +1,12 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { Layer, Rect, Line } from 'react-konva';
-import { useTheme } from 'sancho';
 
 import { IScene } from '..';
 import DraggableStage from './draggableStage';
-import { useTableResolution, useTablePPI } from '../../settings';
-import { Vector2d } from 'konva/types/types';
 import { LayerTypeToComponent, LayerType, ILayer, createNewLayer } from '../layer';
 import { deleteLayer } from '../layer';
 import LayerList from './layerList';
 import { ToolbarPortalProvider } from '../layer/toolbarPortal';
-
-const TableViewOverlay: React.SFC<{ offset: Vector2d, rotation: number, showBorder: boolean, showGrid: boolean }> = ({ offset, rotation, showGrid, showBorder }) => {
-	const theme = useTheme();
-	const [tableResolution] = useTableResolution();
-	const ppi = useTablePPI();
-
-	if (!tableResolution || ppi === null) {
-		return null;
-	}
-
-	const lines = new Array<{ start: Vector2d, end: Vector2d }>();
-
-	if (showGrid) {
-		for (let xOffset = 0; xOffset <= tableResolution.width; xOffset += ppi) {
-			lines.push({
-				start: { x: xOffset, y: 0 },
-				end: { x: xOffset, y: tableResolution.height }
-			})
-		}
-
-		for (let yOffset = 0; yOffset <= tableResolution.height; yOffset += ppi) {
-			lines.push({
-				start: { x: 0, y: yOffset },
-				end: { x: tableResolution.width, y: yOffset }
-			})
-		}
-	}
-
-	return (
-		<Layer>
-			{lines.map((line, i) => (
-				<Line
-					key={i}
-					x={offset.x}
-					y={offset.y}
-					rotation={rotation}
-					points={[line.start.x, line.start.y, line.end.x, line.end.y]}
-					stroke={theme.colors.palette.gray.dark}
-					dash={[1, 1]}
-					strokeWidth={1}
-				/>
-			))}
-			{showBorder &&
-				<Rect
-					x={offset.x}
-					y={offset.y}
-					rotation={rotation}
-					width={tableResolution.width}
-					height={tableResolution.height}
-					stroke={theme.colors.palette.gray.light}
-					dash={[10, 10]}
-					strokeWidth={5}
-					listening={false}
-				/>
-			}
-		</Layer>
-	)
-}
+import TableViewOverlay from './TableViewOverlay';
 
 type Props = { scene: IScene, onUpdate: (scene: IScene) => void };
 const Canvas: React.SFC<Props> = ({ scene, onUpdate }) => {
@@ -167,7 +106,7 @@ const Canvas: React.SFC<Props> = ({ scene, onUpdate }) => {
 						);
 					})
 				}
-				<TableViewOverlay offset={{ x: 0, y: 0 }} rotation={0} showGrid={true} showBorder={true} />
+				<TableViewOverlay offset={scene.table.offset} rotation={0} showGrid={scene.table.displayGrid} showBorder={true} />
 			</DraggableStage>
 
 			<LayerList
