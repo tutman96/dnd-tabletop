cb1673928fa67a3e06e5a5d16d47778485723792
Brandon Tuttle <11356668+tutman96@users.noreply.github.com>
2020-07-26 19:09:11 -0400

    Refactor to combine both image and video assets into one component

--- src/scene/asset/index.tsx
+++ src/scene/asset/index.tsx
@@ -1,6 +1,12 @@
-import { createImageAsset, deleteImageAsset } from "./image";
-import { createVideoAsset, deleteVideoAsset } from "./video";
+import LRU from 'lru-cache';
+import { useState, useEffect } from 'react';
+
+import { getImageSize, getVideoSize } from "./assetSize";
 import { AssetTransform } from "../canvas/transformableAsset";
+import { useAssetFileDatabase } from './storage';
+import { v4 } from 'uuid';
+
+const { storage: fileStorage } = useAssetFileDatabase();
 
 export enum AssetType {
 	IMAGE,
@@ -32,23 +38,93 @@ export function getNewAssets() {
 				const file = files.item(i);
 				if (!file) continue;
 
-				if (file.type.indexOf('image') === 0) {
-					assets.push(await createImageAsset(file));
-				}
-				else if (file.type.indexOf('video') === 0) {
-					assets.push(await createVideoAsset(file));
-				}
+				assets.push(await createAsset(file));
 			}
 			res(assets);
 		}
 	});
 }
 
+export async function createAsset(file: File) {
+	const asset = {
+		id: v4(),
+		type: AssetType.IMAGE,
+		transform: {
+			x: 0, y: 0,
+			width: 0, height: 0,
+			rotation: 0
+		}
+	} as IAsset;
+
+	if (file.type.indexOf('image') === 0) {
+		const res = await getImageSize(file);
+		asset.transform.width = res.width;
+		asset.transform.height = res.height;
+		asset.type = AssetType.IMAGE;
+	}
+	else if (file.type.indexOf('video') === 0) {
+		const res = await getVideoSize(file)
+		asset.transform.width = res.width;
+		asset.transform.height = res.height;
+		asset.type = AssetType.VIDEO;
+	}
+	else {
+		throw new Error('Unknown file type');
+	}
+
+	await fileStorage.setItem(asset.id, file);
+	return asset;
+}
+
 export async function deleteAsset(asset: IAsset) {
-	if (asset.type === AssetType.IMAGE) {
-		await deleteImageAsset(asset);
-	}
-	else if (asset.type === AssetType.VIDEO) {
-		await deleteVideoAsset(asset);
-	}
+	await fileStorage.removeItem(asset.id);
+}
+
+
+type CacheEntry = { file: File, element?: HTMLImageElement | HTMLVideoElement };
+const assetCache = new LRU<string, CacheEntry | null>({
+	max: 1024 * 1024 * 500, // 500 MB
+	length: (entry) => (entry ? entry.file.size : 0),
+	maxAge: 60 * 60 * 1000 // 1 hour
+})
+
+async function getImageElement(file: File) {
+	return new Promise<HTMLImageElement>((res) => {
+		var fr = new FileReader();
+		const img = document.createElement('img') as HTMLImageElement;
+		fr.onload = function () {
+			if (fr.result) {
+				img.src = fr.result as string;
+				res(img);
+			}
+		}
+		fr.readAsDataURL(file);
+	})
+}
+
+async function getVideoElement(file: File) {
+	const video = document.createElement('video');
+	video.src = URL.createObjectURL(file);
+	video.muted = true;
+	video.autoplay = true;
+	video.play();
+	return video;
+}
+
+export function useAssetElement(asset: IAsset) {
+	const [entry, setEntry] = useState<CacheEntry | null | undefined>(assetCache.get(asset.id));
+
+	useEffect(() => {
+		if (entry === undefined) {
+			fileStorage.getItem(asset.id)
+				.then(async (file) => {
+					const element = await (asset.type === AssetType.IMAGE ? getImageElement(file) : getVideoElement(file));
+					const entry = { file, element };
+					assetCache.set(asset.id, entry)
+					setEntry(entry);
+				})
+		}
+	}, [entry, asset.type, asset.id])
+
+	return entry === null ? null : entry?.element;
 }
