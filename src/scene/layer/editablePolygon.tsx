1145d1d6cc7258b31dd949c67858527d1f618077
tutman96 <11356668+tutman96@users.noreply.github.com>
2020-08-12 10:56:37 -0400

    Fixes #31: ability to finish adding a polygon by clicking on first vertex

--- src/scene/layer/editablePolygon.tsx
+++ src/scene/layer/editablePolygon.tsx
@@ -7,12 +7,14 @@ import { Vector2d } from 'konva/types/types';
 import { KonvaEventObject } from 'konva/types/Node';
 import { useKeyPress } from '../../utils';
 
+const ANCHOR_RADIUS = 7;
 const Anchor: React.SFC<{
   firstAnchor: boolean,
   position: Vector2d,
+  onClick: () => void,
   onMove: (position: Vector2d, e: KonvaEventObject<DragEvent>) => void,
   onMoveEnd: () => void
-}> = ({ firstAnchor, position, onMove, onMoveEnd }) => {
+}> = ({ firstAnchor, position, onClick, onMove, onMoveEnd }) => {
   const theme = useTheme();
   const shapeRef = useRef<Konva.Shape>();
   return (
@@ -26,10 +28,15 @@ const Anchor: React.SFC<{
           e.cancelBubble = true;
         }
       }}
+      onMouseUp={(e) => {
+        if (e.evt.button === 0) {
+          onClick();
+        }
+      }}
       sceneFunc={(context, shape) => {
         // custom scene function for rendering an "absolute" radius circle
         const absoluteScale = shape.getAbsoluteScale();
-        const radius = 7 / absoluteScale.x;
+        const radius = ANCHOR_RADIUS / absoluteScale.x;
         context.beginPath();
         context.ellipse(0, 0, radius, radius, 0, 0, Math.PI * 2, false);
         context.closePath();
@@ -71,7 +78,7 @@ interface Props {
   selected: boolean
   onSelected?: () => void
 }
-const EditablePolygon: React.SFC<Props & Omit<Konva.LineConfig, 'points'> & KonvaNodeEvents> = ({ polygon, onUpdate, adding, onAdded, selectable, selected, onSelected, ...lineProps }) => {
+const EditablePolygon: React.SFC<Props & Omit<Konva.LineConfig, 'points'> & KonvaNodeEvents> = ({ polygon, onUpdate, adding, onAdded, selectable, selected, onSelected, closed, ...lineProps }) => {
   const theme = useTheme();
   const groupRef = useRef<Konva.Group>();
 
@@ -103,6 +110,17 @@ const EditablePolygon: React.SFC<Props & Omit<Konva.LineConfig, 'points'> & Konv
         var pos = groupRef.current.getStage()!.getPointerPosition()!;
         var relPos = transform.point(pos);
 
+        const firstVertex = localVerticies[0];
+        if (
+          closed && firstVertex && onAdded &&
+          Math.sqrt((relPos.x - firstVertex.x) ** 2 + (relPos.y - firstVertex.y) ** 2) < ANCHOR_RADIUS / stage.getAbsoluteScale().x
+        ) {
+          polygon.verticies = [...localVerticies, firstVertex];
+          onUpdate(polygon);
+          onAdded();
+          return;
+        }
+
         polygon.verticies = [...localVerticies, relPos];
         onUpdate(polygon);
       }
@@ -113,7 +131,7 @@ const EditablePolygon: React.SFC<Props & Omit<Konva.LineConfig, 'points'> & Konv
         stage.container().style.cursor = 'default';
       };
     }
-  }, [groupRef, adding, localVerticies, onUpdate, polygon])
+  }, [groupRef, adding, localVerticies, onUpdate, onAdded, polygon, closed])
 
   const isEscapePressed = useKeyPress('Escape');
   const isEnterPressed = useKeyPress('Enter');
@@ -157,6 +175,7 @@ const EditablePolygon: React.SFC<Props & Omit<Konva.LineConfig, 'points'> & Konv
     >
       <Line
         {...lineProps}
+        closed={closed}
         points={relativeKonvaCoordinates}
       />
       {selected && (
@@ -173,6 +192,12 @@ const EditablePolygon: React.SFC<Props & Omit<Konva.LineConfig, 'points'> & Konv
             <Anchor
               key={i}
               position={{ x: v.x - groupX, y: v.y - groupY }}
+              onClick={() => {
+                console.log(i, 'clicked');
+                if (adding && i === 0 && onAdded) {
+                  onAdded();
+                }
+              }}
               onMove={(newPos, e) => {
                 const v2 = { x: newPos.x + groupX, y: newPos.y + groupY };
                 localVerticies[i] = v2;
@@ -190,7 +215,7 @@ const EditablePolygon: React.SFC<Props & Omit<Konva.LineConfig, 'points'> & Konv
                 polygon.verticies = localVerticies;
                 onUpdate(polygon);
               }}
-              firstAnchor={adding && i === 0}
+              firstAnchor={closed && adding && i === 0}
             />
           ))}
         </>
