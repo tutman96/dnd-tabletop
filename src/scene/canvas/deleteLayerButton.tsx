7e54d421ed499d3a8d0f78039d1cc514a113f1ac
tutman96 <11356668+tutman96@users.noreply.github.com>
2021-12-28 19:42:21 -0500

    Small cleanup for disabled buttons in the layers list

--- src/scene/canvas/deleteLayerButton.tsx
+++ src/scene/canvas/deleteLayerButton.tsx
@@ -10,30 +10,22 @@ import { TableViewLayer } from "../layer/tableView";
 import ConfirmDialog from "../../partials/confirmDialog";
 
 type Props = { layer?: ILayer; onDelete: () => void; };
-const DeleteLayerButton: React.SFC<Props> = ({ layer, onDelete }) => {
+const DeleteLayerButton: React.FunctionComponent<Props> = ({ layer, onDelete }) => {
   const [showModal, setShowModal] = useState(false);
   const disabled = !layer || layer.id === TableViewLayer.id;
 
-  // Can't have a disabled button be the target of a tooltip
-  if (disabled) {
-    return (
-      <IconButton disabled={true} color="error" size="small">
-        <DeleteOutlinedIcon />
-      </IconButton>
-    )
-  }
-
   return (
     <>
-      <Tooltip title="Delete Layer">
-        <IconButton
-          onClick={() => setShowModal(true)}
-          color="error"
-          size="small"
-        >
+      <IconButton
+        onClick={() => setShowModal(true)}
+        color="error"
+        size="small"
+        disabled={disabled}
+      >
+        <Tooltip title="Delete Layer">
           <DeleteOutlinedIcon />
-        </IconButton>
-      </Tooltip>
+        </Tooltip>
+      </IconButton>
       {layer && <ConfirmDialog
         description={`Are you sure you want to delete '${layer.name}'`}
         open={showModal}
