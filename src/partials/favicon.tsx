fcd1fa51997269f081ecbb841e79ccb859a182f3
tutman96 <11356668+tutman96@users.noreply.github.com>
2021-12-29 14:17:31 -0500

    Fadey boi

--- src/partials/favicon.tsx
+++ src/partials/favicon.tsx
@@ -1,18 +1,42 @@
 import React from 'react';
+
+import Box from '@mui/material/Box'
+
 import { theme } from "../theme";
 
-
+const transition = theme.transitions.create('opacity')
 const FAVICON_SIZE = theme.spacing(4);
 
 type Props = { active: boolean }
-const Favicon: React.FunctionComponent<Props> = ({active}) => {
+const Favicon: React.FunctionComponent<Props> = ({ active }) => {
   return (
-    <img
-      width={FAVICON_SIZE}
-      height={FAVICON_SIZE}
-      src={active ? 'favicon.png' : 'favicon-outlined.png'}
-      alt="home icon"
-    />
+    <Box sx={{
+      width: FAVICON_SIZE, height: FAVICON_SIZE
+    }}>
+      <img
+        width={FAVICON_SIZE}
+        height={FAVICON_SIZE}
+        src='favicon.png'
+        alt="home icon"
+        style={{
+          transition,
+          opacity: active ? 1 : 0,
+          position: 'relative'
+        }}
+      />
+      <img
+        width={FAVICON_SIZE}
+        height={FAVICON_SIZE}
+        src="favicon-outlined.png"
+        alt="home icon"
+        style={{
+          transition,
+          opacity: active ? 0 : 1,
+          position: 'relative',
+          top: -38 // Magic Number
+        }}
+      />
+    </Box>
   )
 }
 export default Favicon;
